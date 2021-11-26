dfa1838125c7926f751457875cbb18ac42a48f39
ViktoriaMolochii <victoriamolochiy@gmail.com>
2021-11-26 18:39:00 +0200

    front-hello-world

---
+++ react_app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          It's a De Havka project !
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
